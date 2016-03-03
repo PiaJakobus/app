c246ce8997fbf40e0424ab2e0d8fa3d7e30dad66
Pia Jakobus <pia.jakobus@googlemail.com>
2016-03-03 10:59:49 +0100

    modified:   person.py

--- person.py
+++ person.py
@@ -1,5 +1,7 @@
+class person(object)
+
 "
-Function: contains all persons wich have an account and can be zugeordnet to at least one household.
+Function: contains all persons wich have an account and can be associated to at least one household.
 "
 
 
@@ -7,5 +9,19 @@ Function: contains all persons wich have an account and can be zugeordnet to at 
 -zugehoeriger household
 -financial situation of single user
 -current shared items of single user
--aufgebrauchte items of user
+-aufgebrauchte items of single user
 "
+
+"
+Methoden():
+-
+"
+
+def __init__(self,associated_household,financial_situation_user, items_user,personal_consumption)
+    self.associated_household = associated_household
+    self.financial_situation = financial_situation
+    self.items_user = items_user
+    self.used_items_user = personal_consumption
+
+
+
